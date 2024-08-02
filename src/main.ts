bce4995c5a0580120f5ef6013597d3b71826cdf2
Vicky-peace <victorpeace47@gmail.com>
2024-08-02 15:30:30 +0300

    added cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import "dotenv/config";
 import { Context } from "hono";
 import {serve} from '@hono/node-server';
 import { jobRouter } from "./jobs/jobs.router";
-
+import {cors} from 'hono/cors'
 //routes
 import { authRouter } from './auth/auth.router';
 import { userRouter } from "./users/user.route";
 const app = new Hono();
 
+
+app.use("*", cors())
 
 app.get("/", async (c) => {
    c.text("Hello World");
